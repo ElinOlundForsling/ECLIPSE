704178a8b25ca0c083e623437dcb5d3c90a57b46
Elin Ölund Forsling <elin@hyena.se>
2020-04-12 21:32:28 +0200

    Making a users fragment

--- app/src/main/java/se/hyena/eclipse/fragment/AccountFragment.kt
+++ app/src/main/java/se/hyena/eclipse/fragment/AccountFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.fragment_account.*
 import kotlinx.android.synthetic.main.fragment_account.view.*
@@ -55,6 +56,9 @@ class AccountFragment : Fragment() {
                     }
                 else
                     FirestoreUtil.updateCurrentUser(editText_name.text.toString(), editText_bio.text.toString(), null)
+
+                val toast = Toast.makeText(this.context, "Saving...", Toast.LENGTH_SHORT)
+                toast.show()
             }
 
             btn_sign_out.setOnClickListener { AuthUI.getInstance()
---
+++ app/src/main/java/se/hyena/eclipse/recyclerview/item/PersonItem.kt
@@ -0,0 +1,32 @@
+package se.hyena.eclipse.recyclerview.item
+
+import android.content.Context
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import kotlinx.android.synthetic.main.fragment_account.*
+import kotlinx.android.synthetic.main.item_person.*
+import kotlinx.android.synthetic.main.item_person.imageView_profile_picture
+import se.hyena.eclipse.R
+import se.hyena.eclipse.glide.GlideApp
+import se.hyena.eclipse.model.User
+import se.hyena.eclipse.util.StorageUtil
+
+
+class PersonItem (
+    val person: User,
+    val userId: String,
+    private val context: Context
+)
+    : Item() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.textView_name.text = person.name
+        viewHolder.textView_bio.text = person.bio
+        if (person.profilePath != null)
+            GlideApp.with(context)
+                .load(StorageUtil.pathToReference(person.profilePath))
+                .placeholder(R.drawable.ic_account_circle_black_24dp)
+                .into(viewHolder.imageView_profile_picture)
+    }
+
+    override fun getLayout() = R.layout.item_person
+}
